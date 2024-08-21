aff48eb6bab9cc64839b16ae122f21b035024667
mac <mac@macs-MacBook-Pro.local>
2024-08-21 14:05:15 +0300

    home routes mainly

---
+++ components/Sidebar.tsx
@@ -0,0 +1,14 @@
+'use client';
+import { usePathname } from 'next/navigation';
+
+const Sidebar = () => {
+    return (
+        <section className="sticky left-0 top-0 flex h-screen w-fit flex-col  justify-between  bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
+            <div className="flex flex-1 flex-col gap-6">
+                Sidebar component
+            </div>
+        </section>
+    );
+};
+
+export default Sidebar;
